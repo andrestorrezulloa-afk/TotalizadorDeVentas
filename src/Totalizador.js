c081c278fbce6e3bc960fb287a545322dfe3861d
André Ermoth <andres.torrez.ulloa@gmail.com>
2026-09-06 23:04:20 -0400

    feature: impuesto aplicado a la compra

--- src/Totalizador.js
+++ src/Totalizador.js
@@ -1,4 +1,16 @@
-function totalizador(cantidad, precio) {
+export function calcularPrecioNeto(cantidad, precio) {
   return cantidad * precio;
 }
-export default totalizador;
+
+
+export function calcularImpuesto(precioNeto, estado) {
+  const impuestosPorEstado = {
+    "UT": 0.0665, 
+    "NV": 0.0800,
+    "TX": 0.0625, 
+    "AL": 0.0400, 
+    "CA": 0.0825  
+  };
+  const tasa = impuestosPorEstado[estado] || 0;
+  return precioNeto * tasa;
+}
--- src/Totalizador.test.js
+++ src/Totalizador.test.js
@@ -1,7 +1,16 @@
-import calcularPrecioNeto from "./Totalizador.js";
+import { calcularPrecioNeto, calcularImpuesto } from "./Totalizador.js";
 
 describe("Totalizador - Precio Neto", () => {
   it("deberia calcular el precio neto multiplicando la cantidad por el precio", () => {
     expect(calcularPrecioNeto(3, 20)).toEqual(60);
+  });
+});
+describe("Totalizador - Impuesto", () => {
+  it("deberia calcular el monto del impuesto para el estado TX (6.25%)", () => {
+    expect(calcularImpuesto(100, "TX")).toEqual(6.25);
+  });
+
+  it("deberia calcular el monto del impuesto para el estado CA (8.25%)", () => {
+    expect(calcularImpuesto(200, "CA")).toEqual(16.5);
   });
 });
